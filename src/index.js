0aa6a3cb0a3c48c98434d986344be567bcc6538d
thanhle <thanhletrantuan@gmail.com>
2023-04-04 18:30:08 +0700

    add prettier

--- src/index.js
+++ src/index.js
@@ -9,22 +9,22 @@ const route = require('./routes');
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-
 //logger
 app.use(morgan('combined'));
 
 //template engine
-app.engine('hbs', engine({
-  extname: '.hbs'
-}));
+app.engine(
+    'hbs',
+    engine({
+        extname: '.hbs',
+    }),
+);
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'resources/views'));
 
 //route init
 route(app);
 
-
-
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
-})
+      app.listen(port, () => {
+            console.log(`Example app listening on port ${port}`);
+});
